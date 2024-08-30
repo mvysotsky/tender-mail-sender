5e448750080a4d7ac2fec249a46851dabe15baa3
mvysotsky <m.vysotskyi@gmail.com>
2024-08-30 18:03:21 +0300

    implement template render

--- service/mail_sender.go
+++ service/mail_sender.go
@@ -1,9 +1,14 @@
 package service
 
 import (
+	"fmt"
 	"log"
 	"mail-sender/db/model"
 	"mail-sender/db/repos"
+	"mail-sender/tools"
+	"strconv"
+
+	"github.com/cbroglie/mustache"
 )
 
 type MailSender interface {
@@ -11,18 +16,24 @@ type MailSender interface {
 }
 
 type mailSender struct {
-	mailQueue repos.DBMailQueue
-	templates repos.DBTemplates
-	tenders   repos.DBTenders
-	users     repos.DBUsers
+	mailQueue       repos.DBMailQueue
+	templates       repos.DBTemplates
+	tenders         repos.DBTenders
+	users           repos.DBUsers
+	senderName      string
+	subserviceAlias string
+	domain          string
 }
 
 func NewMailSender() MailSender {
 	return &mailSender{
-		mailQueue: repos.MailQueue(),
-		templates: repos.Templates(),
-		tenders:   repos.Tenders(),
-		users:     repos.Users(),
+		mailQueue:       repos.MailQueue(),
+		templates:       repos.Templates(),
+		tenders:         repos.Tenders(),
+		users:           repos.Users(),
+		senderName:      tools.GetEnv("OWNER_NAME", "ТОВ АТБ-МАРКЕТ"),
+		subserviceAlias: tools.GetEnv("SUBSERVICE_ALIAS", "ua"),
+		domain:          tools.GetEnv("DOMAIN", ""),
 	}
 }
 
@@ -61,6 +72,7 @@ func (s *mailSender) send(entry repos.MailQueueEntry) (err error) {
 		template string
 		tender   repos.Tender
 		user     repos.User
+		message  string
 	)
 
 	if template, err = s.templates.GetTemplateByID(entry.TemplateID); err != nil {
@@ -72,6 +84,21 @@ func (s *mailSender) send(entry repos.MailQueueEntry) (err error) {
 	}
 
 	if user, err = s.users.GetUser(entry.UserID); err != nil {
+		return err
+	}
+
+	tenderURL := fmt.Sprintf("%s%s/tenders/open_tender/%d", s.domain, s.subserviceAlias, entry.TenderID)
+
+	if message, err = mustache.Render(template, map[string]string{
+		"user_fio":           user.Users.Name,
+		"account_name":       user.Accounts.Name,
+		"account_owner_name": s.senderName,
+		"category":           tender.Categories.Name,
+		"tender_id":          strconv.Itoa(int(entry.TenderID)),
+		"date_start":         tender.DateStart.Format("02.01.2006 15:04"),
+		"date_end":           tender.DateEnd.Format("02.01.2006 15:04"),
+		"tender_link":        tenderURL,
+	}); err != nil {
 		return err
 	}
 
